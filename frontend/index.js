b97d1f7a723a599d03140c6f1cdf82433a24ab90
partjun <partjun@zuminternet.com>
2020-03-24 10:23:19 +0900

    예외처리 추가

--- frontend/index.js
+++ frontend/index.js
@@ -14,35 +14,43 @@ import './monkeypatch';
  * @param initFunction
  */
 export function initializer(initFunction) {
-  let restarted = false;
 
-  Vue.config.errorHandler = function (err, vm, info) {
-    console.error(`An unhandled Vue global error occurred!`, err, vm, info);
-    if (restarted) return;
+  Vue.config.errorHandler = exceptionHandler.bind(this, initFunction);
 
+  try {
+    // 프로젝트 시작
+    initFunction();
+  } catch (e) {
+    exceptionHandler(initFunction, e);
+  }
+
+}
+
+
+
+/**
+ * Vue.js 2 예외 핸들러.
+ * SSR Hydration 및 Global Exception을 핸들링한다
+ */
+let restarted = false;
+function exceptionHandler (initFunction, ...err) {
+  console.error(`An unhandled Vue global error occurred!`, ...err);
+  if (restarted) return;
+
+  // 글로벌 예외 발생시 렌더링된 HTML 모두 제거하고 재시도
+  setTimeout(() => {
+
+    // #app이 이미 존재하면 제거
     const $app = document.querySelector('#app');
-    $app.id = `__${$app.id}`;
-    $app.style.display = 'none';
-    $app.remove();
+    if ($app) $app.remove();
 
-    // 글로벌 예외 발생시 렌더링된 HTML 모두 제거하고 재시도
-    setTimeout(() => {
+    // #app 생성 후 삽입
+    const $div = document.createElement("div");
+    $div.id = 'app';
+    document.body.appendChild($div);
 
-      // #app이 이미 존재하면 다시 제거
-      const $app = document.querySelector('#app').remove();
-      if ($app) $app.remove();
+    restarted = true;
+    initFunction();
 
-      // #app 생성 후 삽입
-      const $div = document.createElement("div");
-      $div.id = 'app';
-      document.body.appendChild($div);
-
-      restarted = true;
-      initFunction();
-
-    }, 100);
-  };
-
-// 프로젝트 시작
-  initFunction();
+  }, 100);
 }
