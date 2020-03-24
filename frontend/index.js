63710f7a9923d3cd29ba44a8437ef0d213939d50
partjun <partjun@zuminternet.com>
2020-03-24 14:29:10 +0900

    예외처리 추가

--- frontend/index.js
+++ frontend/index.js
@@ -13,8 +13,10 @@ let $vm;
 let restarted = false;
 
 /**
- * Global Error를 핸들링한 Vue 실행 함수
- * @param initFunction
+ * Global Error를 핸들링한 Vue 초기화 함수
+ * 
+ * @param initFunction Vue.JS 실행 함수. 
+ *        Router, Vuex 생성 후 Vue 인스턴스를 리턴할 것
  */
 export function initializer(initFunction) {
   Vue.config.errorHandler = exceptionHandler.bind(this, initFunction);
