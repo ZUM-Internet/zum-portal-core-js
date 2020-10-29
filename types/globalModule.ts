9b9d869a4aa8eb751b5ff716897417061d1e37a3
seyoon.kim <ksy7533@zuminternet.com>
2020-10-29 15:03:32 +0900

    merge로 이름 변경

--- types/globalModule.ts
+++ types/globalModule.ts
@@ -4,4 +4,4 @@ import { CookiesStatic } from 'js-cookie';
 export declare const Axios: AxiosStatic;
 export declare const Cookies: CookiesStatic;
 export import fetchJsonp = require('fetch-jsonp');
-export import deepmerge = require('deepmerge');
+export import merge = require('deepmerge');
