66d2549e0e2e23d0ddd2aeb3f27f91b5d902a5cd
partjun <partjun@zuminternet.com>
2021-03-29 11:05:19 +0900

    publish 모드 app.js 파일 관련 수정

--- frontend/monkeypatch/axios/index.js
+++ frontend/monkeypatch/axios/index.js
@@ -26,29 +26,32 @@ if (process.env.NODE_ENV !== 'production' && process.env.ZUM_FRONT_MODE === 'pub
      */
     _$methods[method] = Axios[method];
 
-    Axios[method] = function (url) {
-      var m = _$methods[method];
+    (function (method) {
+      Axios[method] = function (url) {
+        var m = _$methods[method];
 
-      for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
-        args[_key - 1] = arguments[_key];
-      }
+        for (var _len = arguments.length, args = new Array(_len > 1 ? _len - 1 : 0), _key = 1; _key < _len; _key++) {
+          args[_key - 1] = arguments[_key];
+        }
 
-      if (url.indexOf('http') !== -1) {
-        // 외부 호출인 경우 패칭하지 않음
-        return m.apply(void 0, [url].concat(args));
-      }
+        if (url.indexOf('http') !== -1) {
+          // 외부 호출인 경우 패칭하지 않음
+          return m.apply(void 0, [url].concat(args));
+        }
 
-      ; // URL 패칭 이후 js 파일 로드
+        ; // URL 패칭 이후 js 파일 로드
 
-      var paredUrl = url.replace(regex, '');
+        var paredUrl = url.replace(regex, '');
 
-      if (paredUrl.charAt(0) !== '/') {
-        paredUrl = '/' + paredUrl;
-      }
+        if (paredUrl.charAt(0) !== '/') {
+          paredUrl = '/' + paredUrl;
+        }
 
-      ;
-      return m.apply(void 0, [process.env.publicPath + 'stub' + paredUrl].concat(args));
-    };
+        ;
+        return m.apply(void 0, [process.env.publicPath + 'stub' + paredUrl].concat(args));
+      };
+    })(method);
+
   }
 
   ;
