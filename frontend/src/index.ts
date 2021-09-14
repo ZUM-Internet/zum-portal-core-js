f542d57ea2e7f48b5c15e873b71ba3aba261823d
junilhwang <tjsdlf4261@naver.com>
2021-09-14 22:49:56 +0900

    fix: 불필요한 export 제거

--- frontend/src/index.ts
+++ frontend/src/index.ts
@@ -1,6 +1,3 @@
 export * from "./apiClient";
 export * from "./cookieClient";
 export * from "./config";
-
-export { default as fetchJsonp } from "fetch-jsonp";
-export { default as deepmerge } from "deepmerge";
