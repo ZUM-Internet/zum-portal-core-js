18ce7abed1366df55acda60479eca981771e012c
junilhwang <tjsdlf4261@naver.com>
2021-09-24 15:33:32 +0900

    feat: [backend] error log 추가

--- backend/src/base/base.app.container.ts
+++ backend/src/base/base.app.container.ts
@@ -7,7 +7,7 @@ import { NestFactory } from "@nestjs/core";
 import { NestExpressApplication } from "@nestjs/platform-express";
 
 import { CoTracker, NoCacheHtml, ErrorResponse } from "../middleware";
-import { getVersion } from "../util";
+import { getVersion, logger } from "../util";
 
 // 와탭 모니터링 에이전트 등록
 if (process.env.ENABLE_WHATAP === 'true') {
@@ -85,6 +85,8 @@ export abstract class BaseAppContainer {
           // 파비콘 요청인 경우 No Contents 전송
           return res.sendStatus(204);
         }
+
+        logger.error("internal server error", err);
 
         res.sendStatus(500);
       }
