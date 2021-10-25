50d9599e7fe1e81cf75bc732101f07e39b53e57a
dogyeong <dogyeong@zuminternet.com>
2021-10-25 15:28:30 +0900

    refactor: [backend] setup option 수정
    - option객체로 리소스 전체 경로, ejsDelimiter 받도록 수정
    - option으로 받은 경로를 ymlConfiguration에 전달하도록 수정

--- .examples/backend/src/main.ts
+++ .examples/backend/src/main.ts
@@ -1,12 +1,17 @@
-import { ConfigService, NestExpressApplication, BaseAppContainer } from "@zum-portal-core/backend";
-import { AppModule } from "./app.module";
-import "@zum-portal-core/banner";
+import { ConfigService, NestExpressApplication, BaseAppContainer } from '@zum-portal-core/backend';
+import { AppModule } from './app.module';
+import * as path from 'path';
+import '@zum-portal-core/banner';
 
 class AppContainer extends BaseAppContainer {
-  async listen (app: NestExpressApplication) {
+  async listen(app: NestExpressApplication) {
     const configService = app.get(ConfigService);
     await app.listen(configService.get('port'));
   }
 }
 
-new AppContainer().setup(AppModule);
+const options = {
+  resourcePath: path.join(__dirname, '../../resources'),
+};
+
+new AppContainer().setup(AppModule, options);
--- backend/src/base/yml.configuration.ts
+++ backend/src/base/yml.configuration.ts
@@ -1,13 +1,17 @@
 import { join } from 'path';
 const yamlConfig = require('node-yaml-config');
 
+let RESOURCE_PATH = join(process.env.INIT_CWD, '../resources');
+
+export function setYmlResourcePath(path: string) {
+  RESOURCE_PATH = path;
+}
+
 /**
  * YML 파일을 읽어와서 사용하기 위한 설정 함수
  * @param filename
  */
-export function ymlConfiguration (filename: string) {
-  const RESOURCE_PATH = join(process.env.INIT_CWD, '../resources');
-
+export function ymlConfiguration(filename: string) {
   return () => {
     const yamlPath = join(RESOURCE_PATH, filename);
     return yamlConfig.load(yamlPath) as Record<string, any>;
