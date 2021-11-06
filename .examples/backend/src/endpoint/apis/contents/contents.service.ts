bb6cbdcdedbdb037ac68c49a133841c231cc4f00
junil <junil@zuminternet.com>
2021-11-06 17:01:36 +0900

    refactor: [backend] Cache Module 리팩토링

--- .examples/backend/src/endpoint/apis/contents/contents.service.ts
+++ .examples/backend/src/endpoint/apis/contents/contents.service.ts
@@ -16,12 +16,12 @@ export class ContentsService {
     this.APP_API_PATH = configService.get('app-api');
   }
 
-  @ZumCache({ cron: "*/10 * * * * *", logger: console.log })
+  @ZumCache({ ttl: 30, logger: console.log })
   public async getContents () {
     const { base, ['chrome-extension-contents']: chromeExtensionContents } = this.APP_API_PATH;
     const { data } = await this.adapter.get({
       url: base + chromeExtensionContents
     });
-    return Math.random() * 10 > 3 ? data : null;
+    return data;
   }
 }
