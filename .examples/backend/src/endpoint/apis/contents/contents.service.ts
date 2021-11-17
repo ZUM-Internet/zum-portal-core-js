bd181784a03263f0c274476a85ccdf8ad5d36ad4
dogyeong <dogyeong@zuminternet.com>
2021-11-17 18:33:41 +0900

    refactor: [example] 백엔드 코드 리팩토링

--- .examples/backend/src/endpoint/apis/contents/contents.service.ts
+++ .examples/backend/src/endpoint/apis/contents/contents.service.ts
@@ -2,25 +2,23 @@ import {
   ConfigService,
   Injectable,
   ZumCache,
-  ZumProvisionAdapter
-} from "@zum-portal-core/backend";
+  ZumProvisionAdapter,
+  CronExpression,
+} from '@zum-portal-core/backend';
 
 @Injectable()
 export class ContentsService {
   private readonly APP_API_PATH;
 
-  constructor(
-    private readonly adapter: ZumProvisionAdapter,
-    private readonly configService: ConfigService,
-  ) {
+  constructor(private readonly adapter: ZumProvisionAdapter, private readonly configService: ConfigService) {
     this.APP_API_PATH = configService.get('app-api');
   }
 
-  @ZumCache({ cron: "*/10 * * * * *", logger: console.log })
-  public async getContents () {
+  @ZumCache({ cron: CronExpression.EVERY_10_SECONDS, logger: console.log })
+  public async getContents() {
     const { base, ['chrome-extension-contents']: chromeExtensionContents } = this.APP_API_PATH;
     const { data } = await this.adapter.get({
-      url: base + chromeExtensionContents
+      url: base + chromeExtensionContents,
     });
     return Math.random() * 10 > 3 ? data : null;
   }
