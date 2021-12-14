a27360382483350cceaef6449add03e4021dd009
dogyeong <dogyeong@zuminternet.com>
2021-12-14 14:46:07 +0900

    feat: [backend] 도커 이미지 태그파일 찾는 로직 개선
    - 파일을 cwd부터 루트디렉토리까지 순회하면서 찾도록 기능 수정

--- packages/backend/src/middleware/versionResponse.middleware.ts
+++ packages/backend/src/middleware/versionResponse.middleware.ts
@@ -2,16 +2,47 @@ import fs from 'fs';
 import path from 'path';
 import glob from 'glob';
 import { Request, Response } from 'express';
+import { logger } from '../util';
 
-const containerImagePath = path.join(process.env.INIT_CWD, '../container_image_tag.txt');
+/**
+ * 초기 디렉토리부터 루트 디렉토리까지 지정한 파일을 찾는 함수
+ * 없으면 null을 반환한다
+ */
+function findFilePath(initDir: string, filename: string): string | null {
+  if (filename.startsWith('/') || filename.startsWith('..')) {
+    throw new Error('filename must be just a filename');
+  }
+
+  function find(dir: string, name: string): string | null {
+    const filePath = path.join(dir, name);
+
+    function nextLevel() {
+      if (dir === path.resolve('/')) return null;
+      return find(path.dirname(dir), name);
+    }
+
+    try {
+      if (fs.statSync(filePath).isFile()) return filePath;
+      return nextLevel();
+    } catch (e) {
+      return nextLevel();
+    }
+  }
+
+  return find(initDir, filename);
+}
+
+const containerImagePath = findFilePath(process.env.INIT_CWD || process.cwd(), 'container_image_tag.txt');
 
 // 도커 컨테이너 이미지 빌드 시간
-console.log(process.env.INIT_CWD, containerImagePath);
+logger.log(process.env.INIT_CWD, containerImagePath);
 
-const containerImageTag = glob
-  .sync(containerImagePath)
-  .map((src) => fs.readFileSync(src))
-  .join('');
+const containerImageTag = containerImagePath
+  ? glob
+      .sync(containerImagePath)
+      .map((src) => fs.readFileSync(src))
+      .join('')
+  : null;
 
 /**
  * 특정 URL 접속시 컨테이너 이미지 태그 혹은 프로젝트 버전을 응답하는 미들웨어
