060dccf4b4eabf2cb7a057294d2c58598aa404e1
dogyeong <dogyeong@zuminternet.com>
2022-01-10 11:55:11 +0900

    refactor: [frontend] lib 디렉토리 타입스크립트로 재작성

--- packages/frontend/src/lib/cookieClient.ts
+++ packages/frontend/src/lib/cookieClient.ts
@@ -1,23 +1,19 @@
 import Cookies from 'js-cookie';
 
-/**
- * 배포하기 전에 빌드하지 않기 때문에 IE호환되는 문법으로 작성
- */
+const NUMBER_REGEXP = /^\d+$/;
 
-var NUMBER_REGEXP = /^\d+$/;
+export const cookieClient = {
+  get(key: string) {
+    const value = Cookies.get(key);
 
-export var cookieClient = {
-  get: function (key) {
-    var value = Cookies.get(key);
     // Number 파싱
-    if (NUMBER_REGEXP.test(value)) {
+    if (value && NUMBER_REGEXP.test(value)) {
       return parseInt(value);
     }
+
     return value;
   },
-  set: function (key, value, options) {
-    if (!options) options = {};
-
+  set(key: string, value: string, options: Cookies.CookieAttributes = {}) {
     // 개발중일때 domain option 제거
     if (process.env.NODE_ENV !== 'production' && options.domain) {
       delete options.domain;
@@ -27,7 +23,7 @@ export var cookieClient = {
 
     return Cookies.set(key, value, options);
   },
-  remove: function (key, options) {
+  remove(key: string, options?: Cookies.CookieAttributes) {
     Cookies.remove(key, options);
   },
-};
+} as const;
