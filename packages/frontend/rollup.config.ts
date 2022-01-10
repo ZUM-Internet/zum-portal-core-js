82f981441c7c56f6378683fd904b363fcab715d0
dogyeong <dogyeong@zuminternet.com>
2022-01-10 15:36:49 +0900

    Chore: [frontend] rollup으로 lib 번들링

---
+++ packages/frontend/rollup.config.ts
@@ -0,0 +1,24 @@
+import typescript from '@rollup/plugin-typescript';
+import { babel } from '@rollup/plugin-babel';
+
+export default [
+  {
+    input: './src/lib/index.ts',
+    output: {
+      file: './lib/index.js',
+      format: 'es',
+    },
+    external: ['axios', 'js-cookie'],
+    plugins: [
+      typescript({
+        tsconfig: './tsconfig.lib.json',
+        declarationDir: './',
+      }),
+      babel({
+        babelHelpers: 'bundled',
+        exclude: 'node_modules/**',
+        presets: [['@babel/preset-env', { targets: { ie: 10 } }]],
+      }),
+    ],
+  },
+];
