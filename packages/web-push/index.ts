dbfbc1c91df0914ef4006a1a32f2bbf65ff369ec
dogyeong <shroad1802@naver.com>
2022-03-28 11:50:46 +0900

    인터페이스에 옵션 추가

--- packages/web-push/index.ts
+++ packages/web-push/index.ts
@@ -1,20 +1,25 @@
-// zum-news / app.js
+// zum-news/app.js
 import { register } from '@zum-front-core/web-push'
 
 // 이 함수 내부에서는 "/firebase-messaging-sw.js" 서비스 워커를 등록함
 // 그러므로 각 서비스에서는 /firebase-messaging-sw.js 파일이 있어야 함
 register({
-  firebaseConfig: { },
-  onMessageHandler: () => {},
+  firebaseConfig: { }, // optional
+  vapid: '', // optional
+  onMessageHandler: () => {}, // optional
+  onPermissionSuccessed: () => {}, // optional
+  onPermissionRejected: () => {}, // optional
+  onPermissionError: () => {}, // optional
 })
 
 //-----------------------------------------------------------------------
-// zum-news / firebase-messaging-sw.js
+// zum-news/firebase-messaging-sw.js
 import { registerSW } from '@zum-front-core/web-push'
 
 registerSW({
-  firebaseConfig: { },
-  onBackgroundMessageHanlder: () => {},
+  firebaseConfig: { }, // optional
+  vapid: '', // optional
+  onBackgroundMessageHanlder: () => {}, //optional
 })
 
 //-----------------------------------------------------------------------
