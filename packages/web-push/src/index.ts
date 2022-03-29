366c1fa29602973aabc5de72dc59ad340e814527
Seonkyu Kim <somedaycode@gmail.com>
2022-03-29 13:47:42 +0900

    Publish 'web-push' v0.0.2

---
+++ packages/web-push/src/index.ts
@@ -0,0 +1,64 @@
+import { FirebaseOptions, initializeApp } from 'firebase/app';
+import { getToken, getMessaging, onMessage, MessagePayload } from 'firebase/messaging';
+import { onBackgroundMessage } from 'firebase/messaging/sw';
+
+export interface ClientRegisterOptions {
+  firebaseConfig: FirebaseOptions;
+  vapidKey: string;
+  onMessageHandler?: (payload: MessagePayload) => void;
+  onPermissionGranted?: (token: string) => void;
+  onPermissionDenied?: () => void;
+  onPermissionError?: (err: unknown) => void;
+}
+
+export interface ServiceWorkerRegisterOptions extends Pick<ClientRegisterOptions, 'firebaseConfig'> {
+  onBackgroundMessageHanlder?: (payload: MessagePayload) => void;
+}
+
+const defaultMessageHandler = (payload: MessagePayload) => {
+  if (!{}.hasOwnProperty.call(window, 'Notification')) return;
+  if (Notification.permission !== 'granted') return;
+  // eslint-disable-next-line no-new
+  new Notification(payload.notification?.title || '');
+};
+
+const registerClient = (options: ClientRegisterOptions) => {
+  const {
+    firebaseConfig,
+    vapidKey,
+    onMessageHandler,
+    onPermissionDenied,
+    onPermissionError,
+    onPermissionGranted,
+  } = options;
+
+  const firebaseApp = initializeApp(firebaseConfig);
+
+  const messaging = getMessaging(firebaseApp);
+
+  getToken(messaging, { vapidKey })
+    .then((currentToken) => {
+      if (!currentToken) {
+        // 권한 거부한 상태
+        onPermissionDenied?.();
+        return;
+      }
+      // token을 서버로 보내야 함
+      // API 호출 로직
+      onPermissionGranted?.(currentToken);
+    })
+    .catch(onPermissionError);
+
+  onMessage(messaging, onMessageHandler || defaultMessageHandler);
+};
+
+const registerServiceWorker = (options: ServiceWorkerRegisterOptions) => {
+  const { firebaseConfig, onBackgroundMessageHanlder } = options;
+  const messaging = getMessaging(initializeApp(firebaseConfig));
+  onBackgroundMessage(messaging, onBackgroundMessageHanlder || defaultMessageHandler);
+};
+
+export const ZumWebPush = {
+  registerClient,
+  registerServiceWorker,
+};
